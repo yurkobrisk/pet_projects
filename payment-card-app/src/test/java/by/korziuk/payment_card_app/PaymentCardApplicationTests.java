03d44363708f90cfb55faf853e06473a53c1bc71
Yury Korziuk <yurko@tut.by>
2022-10-02 22:24:40 +0300

    Commit 26. Add integration test for the Payment Card Application.

--- payment-card-app/src/test/java/by/korziuk/payment_card_app/PaymentCardApplicationTests.java
+++ payment-card-app/src/test/java/by/korziuk/payment_card_app/PaymentCardApplicationTests.java
@@ -1,5 +1,42 @@
 package by.korziuk.payment_card_app;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = PaymentCardApplication.class)
+@WebAppConfiguration
 public class PaymentCardApplicationTests{
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setUp() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+    }
+
+    @Test
+    void should_return_home () throws Exception {
+        this.mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("clients")));
+    }
 
 }
