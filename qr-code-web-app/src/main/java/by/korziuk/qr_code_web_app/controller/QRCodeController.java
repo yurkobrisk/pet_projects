fcddbdd71f53cf54f2f1a9c7d30902a24ed19e68
Yury Korziuk <yurko@tut.by>
2022-10-12 16:06:47 +0300

    Commit 29. Add library zxing. Add Service QR Code class. Modify Controller class. Add error page.

--- qr-code-web-app/src/main/java/by/korziuk/qr_code_web_app/controller/QRCodeController.java
+++ qr-code-web-app/src/main/java/by/korziuk/qr_code_web_app/controller/QRCodeController.java
@@ -1,22 +1,47 @@
 package by.korziuk.qr_code_web_app.controller;
 
+import by.korziuk.qr_code_web_app.service.QRCodeService;
+import com.google.zxing.WriterException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @Controller
 public class QRCodeController {
 
+    @Autowired
+    QRCodeService qrCodeService;
+
     @GetMapping("/")
     public String getText(Model model){
-        System.out.println("method getText");
-        model.addAttribute("inputText", "This is the default attribute.");
+        model.addAttribute("inputText", "");
+        model.addAttribute("isTextPresent", "false");
         return "index.html";
     }
 
     @PostMapping("/")
-    public String setQRCode(@ModelAttribute(name = "inputText") String inputText){
-        System.out.println("method setQRCode");
+    public String setQRCode(
+            Model model,
+            @ModelAttribute(name = "inputText") String inputText){
+        try {
+            if (!inputText.isEmpty()){
+                String imageBLOB = qrCodeService.createImageQR(inputText);
+                model.addAttribute("imageBLOB", imageBLOB);
+                model.addAttribute("isTextPresent", "true");
+            } else {
+                model.addAttribute("isTextPresent", "false");
+            }
+        } catch (WriterException | IOException e) {
+            throw new RuntimeException("The image writer has error");
+        }
         return "index.html";
+    }
+
+    @GetMapping("/error")
+    public String error(){
+        return "error.html";
     }
 }
