7171a084339e7e943b0e13d93a483d5a19adcac1
Yury Korziuk <yurko@tut.by>
2023-03-28 16:54:14 +0300

    Commit 59. Init Tasty Meal App. Add Spring 3 [web, jpa, sql connector], Java 17.

---
+++ tasty-meals-app/src/main/java/by/korziuk/tasty_meals_app/TastyMealsApplication.java
@@ -0,0 +1,13 @@
+package by.korziuk.tasty_meals_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyMealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyMealsApplication.class, args);
+	}
+
+}
---
+++ tasty-meals-app/src/test/java/by/korziuk/tasty_meals_app/TastyMealsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.korziuk.tasty_meals_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyMealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
