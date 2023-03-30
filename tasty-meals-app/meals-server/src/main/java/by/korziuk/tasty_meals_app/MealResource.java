d53f068012afdb3f8dc6c796d584dfaa4f1cdc01
Yury Korziuk <yurko@tut.by>
2023-03-30 21:00:36 +0300

    Commit 64. Refactor and create module.

--- tasty-meals-app/meals-server/src/main/java/by/korziuk/tasty_meals_app/MealResource.java
+++ tasty-meals-app/meals-server/src/main/java/by/korziuk/tasty_meals_app/MealResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/meals")
+@RequestMapping("/meal")
 public class MealResource {
     private final MealService mealService;
 
