e406ed1ed8517effef2a6d9ba1890e4fc9ad0f84
Yury Korziuk <yurko@tut.by>
2023-03-31 12:51:48 +0300

    Commit 65. Create Angular project. Create service meal.

---
+++ tasty-meals-app/tastymealsapp/src/app/meal.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Meal } from './meal';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MealService {
+    private apiServerUrl = '';
+
+    constructor(private http: HttpClient) {}
+
+    public getMeals(): Observable<Meal[]> {
+        return this.http.get<Meal[]>(`${this.apiServerUrl}/meal/all`);
+    }
+
+    public addMeal(meal: Meal): Observable<Meal> {
+        return this.http.post<Meal>(`${this.apiServerUrl}/meal/add`, meal);
+    }
+
+    public updateMeal(meal: Meal): Observable<Meal> {
+        return this.http.put<Meal>(`${this.apiServerUrl}/meal/update`, meal);
+    }
+
+    public deleteMeal(mealId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/meal/delete/${mealId}`);
+    }
+}
---
+++ tasty-meals-app/tastymealsapp/src/app/meal.ts
@@ -0,0 +1,8 @@
+export interface Meal {
+    id : number;
+    name : string;
+    rating : number;
+    description : string;
+    price : string;
+    imageUrl : string;
+}
