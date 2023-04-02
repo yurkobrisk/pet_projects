48a69bb481d5772a4aa9a8a305128f0ef3952c91
Yury Korziuk <yurko@tut.by>
2023-04-02 20:05:38 +0300

    Commit 67. Connect java rest with ui.

--- tasty-meals-app/meals-server/src/main/java/by/korziuk/tasty_meals_app/MealResource.java
+++ tasty-meals-app/meals-server/src/main/java/by/korziuk/tasty_meals_app/MealResource.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/meal")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class MealResource {
     private final MealService mealService;
 
--- tasty-meals-app/tastymealsapp/src/environments/environments.ts
+++ tasty-meals-app/tastymealsapp/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl:'https//localhost:8080'
+  apiBaseUrl:'http://localhost:8080'
 };
