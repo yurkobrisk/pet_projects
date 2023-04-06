7c9049c61759e326ce221ddf0eef883165ae5973
Yury Korziuk <yurko@tut.by>
2023-04-06 23:04:22 +0300

    Commit 69. Add modal method to App-component. Add different modal windows to html.

--- tasty-meals-app/tastymealsapp/src/app/app.component.ts
+++ tasty-meals-app/tastymealsapp/src/app/app.component.ts
@@ -28,5 +28,23 @@ export class AppComponent implements OnInit{
     );
   }
 
+  public onOpenModal(meal: Meal, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addMealModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateMealModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteMealModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
 
 }
